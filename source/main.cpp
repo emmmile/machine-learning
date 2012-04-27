3d3c9b9c2419690901238edd74c6eb297230d668
Emilio <emilio.deltessa@gmail.com>
2012-04-27 14:27:54 +0200

    added a benchmark to the main file

--- source/main.cpp
+++ source/main.cpp
@@ -4,22 +4,11 @@ using namespace std;
 
 
 int main() {
-
-	/* test the new data structures
-	int array[10];fill( array, array + 10, 9999 );
-	typeholder<int> a( 3.2 );
-	a = 2;
-	state<uchar> b = 7;
-	cout << a << " " << array[b] << endl;
-
-	for ( state<uchar,7> i = 0; i < 10; ++i ) {
-		cout << i << endl;
-	}*/
-
 	Random gen;
-	living_tm<3,8> t (gen);
-	t.do_nsteps( 100000000);
+	living_tm<3, 8> t (gen);
 
+	cout << t;
+	t.do_nsteps( 200000000 );
 
 	return 0;
 }
