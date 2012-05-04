b9cde257ccdc198660f4bfcdebd00b988801b0f8
lipsum <jeanflorent@no-log.org>
2012-05-04 12:24:53 +0200

    Crossover and mutation in population. Functoin to remove halted machines.

--- source/ev_machine.hpp
+++ source/ev_machine.hpp
@@ -37,6 +37,14 @@ public:
     return *machines[i];
   }
 
+  inline ltm_type at(const uint i) {
+    return *machines[i]
+  }
+
+  inline size_type size() {
+    return machines.size();
+  }
+
   void push_back(const ltm_type& ltm) {
     // add the living_tm ltm in the population (at the end of the array)
     ltm_type new_machine = new ltm_type (ltm);
@@ -45,15 +53,36 @@ public:
 
   void erase(const uint i) {
     // remove the i-th machine from the population
-    assert(i < machines.size());
-    delete *machines[i];
+    assert(i < size());
+    delete at(i);
     machines.erase(i);
   }
 
   void nsteps_for_all(_int n) {
     // do n step on all machines of the population
-    for(int i = 0; i < machines.size() ; ++i)
-      *machines[i].do_nsteps(n);
+    for(int i = 0; i < size() ; ++i)
+      at(i).do_nsteps(n);
+  }
+
+  void erase_halt() {
+    // remove from the population all already halted machines
+    for(int i = 0; i < size() ; ++i)
+      if (at(i).get_state() == NStates)
+	erase(i);
+  }
+
+  void mutate(Random& gen) {
+    at(gen.integer() % size()).mutate(gen);
+    // ^- I hope that the max of gen.integer() is greater than size()...
+    // not equiprobabilistic since size does not always divide
+    // the max of gen.integer()
+  }
+
+  void crossover ( turing_machine& a, Random& gen, crossover_type type = TWO_POINT ) {
+    int i_ltm1 = gen.integer() % size(),
+      i_ltm2 = gen.integer() % size();
+    at(i_ltm1).crossover(at(i_ltm2),gen,type);
+
   }
 
   friend class boost::serialization::access;
