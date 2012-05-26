dfeab193799e2d822fc538b94fe2b7f6526f7920
lipsum <jeanflorent@no-log.org>
2012-05-26 11:30:38 +0200

    experiments.cpp is the main we use for experiments.

---
+++ source/experiments.cpp
@@ -0,0 +1,6 @@
+/*****************************************************************************
+ * experiments.cpp                                                           *
+ * principal program we use to launch experiments on BB and EC               *
+ * main.cpp is used for tests and bug solving                                *
+ *****************************************************************************/
+
