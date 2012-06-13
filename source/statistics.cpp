3cebeafe7773b33ce41172d3714bf2931c8294ec
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2012-06-13 17:11:06 +0200

    re-executed tests, added other plots

--- source/statistics.cpp
+++ source/statistics.cpp
@@ -9,21 +9,22 @@ using namespace std;
 int main() {
   typedef living_tm<N, M> ltm;
 
-  double pc = 0.9;
+  double pc = 0.0;
   double pm = 0.8;
-  int totalTrials = 500;
-  int generations[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+  int totalTrials = 200;
+  int generations[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 
+                        250, 500, 750, 1000, 2500, 7500 };
   int plotPoints = sizeof(generations) / sizeof(int);
 
   printf( "generations\tpc\tpm\taverageSteps\taverageExplored\tbestFound%%\n" );
-  for ( int j = 0; j < plotPoints; ++j ) {
+  for ( int j = plotPoints - 1; j >= 0; --j ) {
     double average = 0;
     double averageExplored = 0;
     int timesBestFound = 0;
 
     for ( int i = 0; i < totalTrials; i++ ) {
       // the seed changes at every trial
-      population<ltm> p( i + (j << 4), 400, true, pc, pm );
+      population<ltm> p( i + (generations[j] << 4), 400, false, pc, pm );	// remember to put true or false depending on the case, and eventually change living-tm.hpp
       p.run( generations[j] );
       if ( !p.get_best().get_state().ishalt() ) {
         cout << "NOT Halted!!!\n";
